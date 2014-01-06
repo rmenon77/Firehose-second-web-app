7855cf67860611877b9cdd29f1b33f2f830d4c6f
Ruby <ruby@dhcp-168-105-87-80.wireless.manoa.hawaii.edu>
2014-01-05 17:34:40 -1000

    embed you tube video in my second web app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Embeddr::Application.config.secret_key_base = '02cf7f1a7cee7be753bbc7f8a7ba8a507353ef06990072bdbdf7b039b04f758ce16ee06575f41e783a5aac09f877477d32ef8deeb9d054979a360c92e585beb3'
